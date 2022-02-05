637aed6aded98fe763324baafa176d31288c901e
DIVYANSH856 <74702000+DIVYANSH856@users.noreply.github.com>
2022-02-06 03:22:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-var mydata = JSON.parse(data);
-function displayData(){
-  alert(mydata[0].name);
-  alert(mydata[0].age);
-  alert(mydata[1].name);
-  alert(mydata[1].age);
+function displayData() {
+  var x = document.getElementById("right");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    const inframe = document.querySelector("#inframe");
+    const inputvalue = document.querySelector("#inputvalue");
+    const downloadbtn = document.querySelector("#Download");
+    inframe.src=inputvalue.value;
+    downloadbtn.href=inputvalue.value;
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+function myFunction(){
+  let dataList = document.querySelector( "#names" );
+  let optionValues = ["Ujjwal_singh.json","Vishal_grover.json" ];
+  // add each element of <optionValues> array
+  for( let optionValue of optionValues )
+  {
+      let newOption = document.createElement( "option" );
+      newOption.value = optionValue;
+      dataList.appendChild(newOption);
+  }
 }
